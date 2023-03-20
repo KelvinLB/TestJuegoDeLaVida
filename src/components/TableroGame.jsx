b78c46609495e423b1ee9450228a1388e5e1fa5e
KelvinLB <kelvinlbx1@gmail.com>
2023-03-19 19:49:44 -0600

    Se agrega la lib bootstrap y se crea el componente Tablero

---
+++ src/components/TableroGame.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function TableroGame() {
+  return (
+    <table className="table table-bordered">
+      <tr>
+        <td>Elemento 1,1</td>
+        <td>Elemento 1,2</td>
+        <td>Elemento 1,3</td>
+      </tr>
+      <tr>
+        <td>Elemento 2,1</td>
+        <td>Elemento 2,2</td>
+        <td>Elemento 2,3</td>
+      </tr>
+      <tr>
+        <td>Elemento 3,1</td>
+        <td>Elemento 3,2</td>
+        <td>Elemento 3,3</td>
+      </tr>
+    </table>
+  );
+}
